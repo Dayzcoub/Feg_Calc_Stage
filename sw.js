ee607cf7218b9dad1d688379971238da0148bc10
Dayzcoub <77221495+Dayzcoub@users.noreply.github.com>
2026-05-04 11:20:56 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,16 @@
-const CACHE_NAME = "feg-stage-v1";
+const CACHE_NAME = "feg-stage-pro-v1";
 
-const urlsToCache = [
-  "/",
-  "/index.html"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll([
+      "/",
+      "/index.html"
+    ]))
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
